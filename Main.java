ab21216daabfc1d07d4a9b6a0d7dcccd8e2b38c3
explodeTheComputer <dnfjddk2@gmail.com>
2025-02-26 12:52:35 +0900

    sdafadsf

--- Main.java
+++ Main.java
@@ -2,36 +2,100 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
 
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
+
+    static final int INF = Integer.MAX_VALUE;
+    static int end;
+
     public static void main(String[] args)throws IOException{
 
         st = new StringTokenizer(br.readLine());
         final int n = Integer.parseInt(st.nextToken());
+        final int start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
         final int m = Integer.parseInt(st.nextToken());
 
-        final long[] sum = new long[n+1];
-        final long[] remain = new long[m];
+        final int[] dist = new int[n];
+        final Edge[] edges = new Edge[m];
+        final int[] profit = new int[n];
+        final boolean[] isCycle = new boolean[n];
+        
 
-        st = new StringTokenizer(br.readLine());
-        for(int i=1;i<=n;i++){
-            sum[i] = sum[i-1]+Integer.parseInt(st.nextToken());
-            final int idx = (int)(sum[i]%m);
-            remain[idx]++;
-        }
-
-        long cnt = remain[0];
+        Arrays.fill(dist,-INF);
+        
         for(int i=0;i<m;i++){
-            cnt+=remain[i]*(remain[i]-1)/2;
+            st = new StringTokenizer(br.readLine());
+            final int s = Integer.parseInt(st.nextToken());
+            final int e = Integer.parseInt(st.nextToken());
+            final int p = Integer.parseInt(st.nextToken());
+            edges[i] = new Edge(s,e,p);
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n;i++)
+        profit[i]=Integer.parseInt(st.nextToken());
+        dist[start]=profit[start];
+        
+        for(int i=0;i<n-1;i++){
+            for(final var edge:edges){
+                final int s = edge.s;
+                final int e = edge.e;
+                final int p = edge.p;
+                if(dist[s]!=-INF && dist[s]-p+profit[e]>dist[e]){
+                    dist[e] = dist[s]-p+profit[e];
+                }
+            }
+        }
+        if(dist[end]==-INF){
+            System.out.println("gg");
+            System.exit(0);
         }
 
-        sb.append(cnt);
+        for(final var edge:edges){
+            final int s = edge.s;
+            final int e = edge.e;
+            final int p = edge.p;
+            if(dist[s]!=-INF &&dist[s]-p+profit[e]>dist[e]){
+                isCycle[s]=true;
+                isCycle[e]=true;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(isCycle[i]){
+                boolean[] vist = new boolean[n];
+                dfs(vist,edges,i);
+            }
+        }
+
+        sb.append(dist[end]);
 
         System.out.println(sb);
         return;
+    }
+    static class Edge{
+        final int s,e,p;
+        public Edge(int s,int e,int p){
+            this.s=s;
+            this.e=e;
+            this.p=p;
+        }
+    }
+    static void dfs(boolean[] vist,Edge[] edges,final int v){
+        vist[v] = true;
+        for(final var edge:edges){
+            if(edge.s==v && !vist[edge.e]){
+                if(edge.e==end){
+                    System.out.println("Gee");
+                    System.exit(0);
+                }
+                dfs(vist,edges,edge.e);
+            }
+        }
     }
 }
