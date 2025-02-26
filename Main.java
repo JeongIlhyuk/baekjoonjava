c19a35b496c446500497073df3466bbca877fe2b
explodeTheComputer <dnfjddk2@gmail.com>
2025-02-26 11:38:25 +0900

    asdfasdfasf

--- Main.java
+++ Main.java
@@ -2,89 +2,36 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
-import java.util.Arrays;
+
 
 public class Main{
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
-    static int n;
+    static StringTokenizer st;
     public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int INF = 100000;
 
         st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
+        final int n = Integer.parseInt(st.nextToken());
         final int m = Integer.parseInt(st.nextToken());
 
-        int[] distance = new int[n];
-        final int[][] edgeArr = new int[m][3];
-        int[] route = new int[n+1];
-        boolean[] isCycle = new boolean[n];
+        final long[] sum = new long[n+1];
+        final long[] remain = new long[m];
 
-        Arrays.fill(distance,-INF);
-        distance[0]=0;
-
-        for(int j=0;j<m;j++){
-            st = new StringTokenizer(br.readLine());
-            edgeArr[j][0] = Integer.parseInt(st.nextToken());
-            edgeArr[j][1] = Integer.parseInt(st.nextToken());
-            edgeArr[j][2] = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for(int i=1;i<=n;i++){
+            sum[i] = sum[i-1]+Integer.parseInt(st.nextToken());
+            final int idx = (int)(sum[i]%m);
+            remain[idx]++;
         }
 
-        for(int j=0;j<n-1;j++){
-            for(final int[] edge:edgeArr){
-                final int s = edge[0];
-                final int e = edge[1];
-                final int t = edge[2];
-
-                if(distance[s-1]!=-INF &&distance[s-1]+t>distance[e-1]){
-                    distance[e-1]=distance[s-1]+t;
-                    route[e]=s;
-                }
-            }
+        long cnt = remain[0];
+        for(int i=0;i<m;i++){
+            cnt+=remain[i]*(remain[i]-1)/2;
         }
-        
-        for(final int[] edge:edgeArr){
-            final int s = edge[0];
-            final int e = edge[1];
-            final int t = edge[2];
 
-            if(distance[s-1]!=-INF &&distance[s-1]+t>distance[e-1]){
-                isCycle[s-1]=true;
-            }
-        }
-        
-        for(int i=0;i<n;i++)
-            if(isCycle[i]){
-                boolean[] visited = new boolean[n];
-                dfs(visited,edgeArr,i);
-            }
-
-        printResult(route,n);
+        sb.append(cnt);
 
         System.out.println(sb);
-        return;
-    }
-    static void printResult(final int[] route,final int idx){
-        if(idx==1){
-            sb.append("1 ");
-            return;
-        }
-        printResult(route,route[idx]);
-        sb.append(idx).append(' ');
-    }
-    static void dfs(boolean[] visited,final int[][] edgeArr,final int vertex){
-        visited[vertex]=true;
-        for(final int[] edge:edgeArr){
-            if(edge[0]-1==vertex&&!visited[edge[1]-1]){
-                if(edge[1]==n){
-                    System.out.println(-1);
-                    System.exit(0);
-                }
-                dfs(visited,edgeArr,edge[1]-1);
-            }
-        }
         return;
     }
 }
