59d1889a2592b0291ba41144bcfec7979eae087a
explodeTheComputer <dnfjddk2@gmail.com>
2025-02-27 15:48:02 +0900

    asdfasdf

--- Main.java
+++ Main.java
@@ -1,100 +1,81 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+
 import java.util.StringTokenizer;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Arrays;
 
+import java.util.Queue;
 
 public class Main{
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-
-    static final int INF = Integer.MAX_VALUE;
-    static int end;
-
+    
     public static void main(String[] args)throws IOException{
+        final int INF = Integer.MAX_VALUE;
+        final int n = Integer.parseInt(br.readLine());
+        final int m = Integer.parseInt(br.readLine());
+        final int[][] graph = new int[n][n];
+        final int[] dp = new int[n];
+        final int[] dp2 = new int[n];
 
-        st = new StringTokenizer(br.readLine());
-        final int n = Integer.parseInt(st.nextToken());
-        final int start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
-        final int m = Integer.parseInt(st.nextToken());
+        for(int i=0;i<n;i++)
+            Arrays.fill(graph[i],INF);
+        for(int i=0;i<n;i++)
+            graph[i][i]=0;
+        Arrays.fill(dp,INF);
 
-        final long[] dist = new long[n];
-        final long[] profit = new long[n];
-        final Edge[] edges = new Edge[m];
-        final boolean[] isCycle = new boolean[n];
-        
-
-        Arrays.fill(dist,-INF);
-        
+        int[] indegree = new int[n];
         for(int i=0;i<m;i++){
             st = new StringTokenizer(br.readLine());
             final int s = Integer.parseInt(st.nextToken());
             final int e = Integer.parseInt(st.nextToken());
-            final int p = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(s,e,p);
+            final int t = Integer.parseInt(st.nextToken());
+            graph[s-1][e-1]=t;
+            dp[e-1]=t;
+            indegree[e-1]++;
         }
-        
+
         st = new StringTokenizer(br.readLine());
+        final int start = Integer.parseInt(st.nextToken());
+        dp[start-1]=0;
+        final int end = Integer.parseInt(st.nextToken());
+
+        Queue<Integer> queue = new LinkedList<>();
         for(int i=0;i<n;i++)
-        profit[i]=Integer.parseInt(st.nextToken());
-        dist[start]=profit[start];
-        
-        for(int i=0;i<n-1;i++){
-            for(final var edge:edges){
-                final int s = edge.s;
-                final int e = edge.e;
-                final long p = edge.p;
-                if(dist[s]!=-INF && dist[s]-p+profit[e]>dist[e]){
-                    dist[e] = dist[s]-p+profit[e];
+            if(indegree[i]==0){
+                queue.offer(i);
+            }
+
+        while(!queue.isEmpty()){
+            final int curr = queue.poll();
+            if(curr==start-1)dp2[curr]=1;
+            for(int i=0;i<n;i++)
+                if(graph[curr][i]<INF && curr!=i){
+                    if(--indegree[i] ==0)queue.offer(i);
+                    if(dp[curr]<INF){
+                        if(dp[i]==INF){
+                            dp[i]=dp[curr]+graph[curr][i];
+                            dp2[i]+=dp2[curr];
+                        }else{
+                            if(dp[i]<dp[curr]+graph[curr][i]){
+                                dp[i]=dp[curr]+graph[curr][i];
+                                dp2[i]=dp2[curr];
+                            }else if(dp[i]==dp[curr]+graph[curr][i]){
+                                dp2[i]+=dp2[curr];
+                            }
+                        }
+                    }
                 }
-            }
-        }
-        if(dist[end]==-INF){
-            System.out.println("gg");
-            System.exit(0);
         }
 
-        for(final var edge:edges){
-            final int s = edge.s;
-            final int e = edge.e;
-            final long p = edge.p;
-            if(dist[s]!=-INF &&dist[s]-p+profit[e]>dist[e]){
-                isCycle[s]=true;
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(isCycle[i]){
-                boolean[] vist = new boolean[n];
-                dfs(vist,edges,i);
-            }
-        }
-
-        sb.append(dist[end]);
+        sb.append(dp[end-1]+"\n"+dp2[end-1]+"\n");
 
         System.out.println(sb);
         return;
-    }
-    static class Edge{
-        final int s,e,p;
-        public Edge(int s,int e,int p){
-            this.s=s;
-            this.e=e;
-            this.p=p;
-        }
-    }
-    static void dfs(boolean[] vist,Edge[] edges,final int v){
-        vist[v] = true;
-        if(v==end){
-            System.out.println("Gee");
-            System.exit(0);
-        }
-        for(final var edge:edges){
-            if(edge.s==v && !vist[edge.e]){
-                dfs(vist,edges,edge.e);
-            }
-        }
     }
 }
