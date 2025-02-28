f2adab58dbeaaa44e6516425f9b5cb18c3c9c5b8
explodeTheComputer <dnfjddk2@gmail.com>
2025-02-28 13:39:05 +0900

    sfsdfaff

--- Main.java
+++ Main.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 
 import java.util.StringTokenizer;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import java.util.Stack;
 import java.util.Queue;
 
 public class Main{
@@ -15,67 +16,115 @@ public class Main{
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
     
+    static ArrayList<Integer>[] graph;
     public static void main(String[] args)throws IOException{
-        final int INF = Integer.MAX_VALUE;
-        final int n = Integer.parseInt(br.readLine());
-        final int m = Integer.parseInt(br.readLine());
-        final int[][] graph = new int[n][n];
-        final int[] dp = new int[n];
-        final int[] dp2 = new int[n];
-
-        for(int i=0;i<n;i++)
-            Arrays.fill(graph[i],INF);
-        for(int i=0;i<n;i++)
-            graph[i][i]=0;
-        Arrays.fill(dp,INF);
-
-        int[] indegree = new int[n];
-        for(int i=0;i<m;i++){
-            st = new StringTokenizer(br.readLine());
-            final int s = Integer.parseInt(st.nextToken());
-            final int e = Integer.parseInt(st.nextToken());
-            final int t = Integer.parseInt(st.nextToken());
-            graph[s-1][e-1]=t;
-            dp[e-1]=t;
-            indegree[e-1]++;
-        }
-
+        //노드 수
         st = new StringTokenizer(br.readLine());
-        final int start = Integer.parseInt(st.nextToken());
-        dp[start-1]=0;
-        final int end = Integer.parseInt(st.nextToken());
-
-        Queue<Integer> queue = new LinkedList<>();
+        final int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        //간선 정보
+        graph=new ArrayList[n];
         for(int i=0;i<n;i++)
-            if(indegree[i]==0){
-                queue.offer(i);
-            }
-
-        while(!queue.isEmpty()){
-            final int curr = queue.poll();
-            if(curr==start-1)dp2[curr]=1;
-            for(int i=0;i<n;i++)
-                if(graph[curr][i]<INF && curr!=i){
-                    if(--indegree[i] ==0)queue.offer(i);
-                    if(dp[curr]<INF){
-                        if(dp[i]==INF){
-                            dp[i]=dp[curr]+graph[curr][i];
-                            dp2[i]+=dp2[curr];
-                        }else{
-                            if(dp[i]<dp[curr]+graph[curr][i]){
-                                dp[i]=dp[curr]+graph[curr][i];
-                                dp2[i]=dp2[curr];
-                            }else if(dp[i]==dp[curr]+graph[curr][i]){
-                                dp2[i]+=dp2[curr];
-                            }
-                        }
-                    }
-                }
+            graph[i] = new ArrayList<>();
+        while(m-->0){
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e= Integer.parseInt(st.nextToken());
+            graph[s-1].add(e-1);
+        }
+        //ATM 현금 보유량
+        final FindScc fs = new FindScc(n);
+        for(int i=0;i<n;i++)
+            fs.atm[i] = Integer.parseInt(br.readLine());
+        //출발 장소,레스토랑 수
+        st = new StringTokenizer(br.readLine());
+        final int s= Integer.parseInt(st.nextToken());
+        int p = Integer.parseInt(st.nextToken());
+        //레스토랑 노드 정보
+        st = new StringTokenizer(br.readLine());
+        while(p-->0){
+            final int num = Integer.parseInt(st.nextToken());
+            fs.isRestaurant[num-1]=true;
         }
 
-        sb.append(dp[end-1]+"\n"+dp2[end-1]+"\n");
+        fs.find();
 
+        //위상 정렬
+        
         System.out.println(sb);
+
+        br.close();
         return;
+    }
+    static class FindScc{
+        private final int n;
+        private int leastId=0;
+        
+        private int sccNum = 0;
+
+        private final int[] id;
+        private final int[] lowest;
+        private final boolean[] isStack;
+
+        private final int[] sccId;
+        
+        private final int[] atm;
+        private final boolean[] isRestaurant;
+        
+        private final Stack<Integer> stack = new Stack<>();
+        
+        private final ArrayList<ArrayList<Integer>> sccList=new ArrayList<>();
+        private final ArrayList<Integer> sccAtmSum=new ArrayList<>();
+        private final ArrayList<Boolean> hasRestaurant=new ArrayList<>();
+        public FindScc(int n){
+            this.n=n;
+            lowest = new int[n];
+            isStack=new boolean[n];
+            id=new int[n];
+            
+            sccId=new int[n];
+            atm = new int[n];
+            isRestaurant = new boolean[n];
+            Arrays.fill(id,-1);
+        }
+        private void dfs(final int v){
+            id[v]=lowest[v]=leastId++;
+            stack.push(v);
+            isStack[v]=true;
+            for(final int adjV:graph[v]){
+                if(id[adjV]==-1){
+                    dfs(adjV);
+                    lowest[v]=Math.min(lowest[v],lowest[adjV]);
+                }else if(isStack[adjV]){
+                    lowest[v]=Math.min(lowest[v],id[adjV]);
+                }
+            }
+    
+            if(lowest[v]==id[v]){
+                ArrayList<Integer> scc = new ArrayList<>();
+
+                int s,GMoney=0;
+                boolean isR = false;
+                do{
+                    s = stack.pop();
+                    scc.add(s);
+                    isStack[s]=false;
+
+                    sccId[s]=sccNum;
+
+                    GMoney += atm[s];
+                    if(isRestaurant[s])isR = true;
+                }while(s!=v);
+                sccList.add(scc);
+
+                hasRestaurant.add(isR);
+                sccAtmSum.add(GMoney);
+                sccNum++;
+            }
+        }
+        public void find(){
+            for(int i=0;i<n;i++)
+                if(id[i]==-1)this.dfs(i);
+        }
     }
 }
